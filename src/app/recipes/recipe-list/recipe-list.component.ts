0d9fceb0321123a0386d7dd914d893e5da1b4f41
rehab.samy <rehab.samy@xlab-group.com>
2022-08-15 11:44:04 +0200

    Constructing recipes and shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes : Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1oOPxgHRk5j1dQtgI0UBK_SS1EUQaeOv9mQ&usqp=CAU'),
+    new Recipe('A Test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1oOPxgHRk5j1dQtgI0UBK_SS1EUQaeOv9mQ&usqp=CAU')
+  ]
   constructor() { }
 
   ngOnInit(): void {
