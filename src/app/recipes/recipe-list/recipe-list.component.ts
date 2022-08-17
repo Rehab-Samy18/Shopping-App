85bbf73a50117518a9e99d52d3f58c2d7390e95f
rehab.samy <rehab.samy@xlab-group.com>
2022-08-17 16:18:59 +0200

    Components & Databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes : Recipe[] = [
     new Recipe('A Test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1oOPxgHRk5j1dQtgI0UBK_SS1EUQaeOv9mQ&usqp=CAU'),
-    new Recipe('A Test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1oOPxgHRk5j1dQtgI0UBK_SS1EUQaeOv9mQ&usqp=CAU')
+    new Recipe('Another Test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1oOPxgHRk5j1dQtgI0UBK_SS1EUQaeOv9mQ&usqp=CAU')
   ]
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
