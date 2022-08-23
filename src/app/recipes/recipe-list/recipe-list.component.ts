71962a28f69e365dc0901450eb2ee88850b6f1da
rehab.samy <rehab.samy@xlab-group.com>
2022-08-23 12:27:11 +0200

    Adding recipe and shopping list services

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,16 +8,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes : Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1oOPxgHRk5j1dQtgI0UBK_SS1EUQaeOv9mQ&usqp=CAU'),
-    new Recipe('Another Test Recipe','This is simply a test','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1oOPxgHRk5j1dQtgI0UBK_SS1EUQaeOv9mQ&usqp=CAU')
-  ]
-  constructor() { }
+  recipes:Recipe[];
+  constructor(private recipeService:RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
-  onRecipeSelected(recipe:Recipe){
-    this.recipeWasSelected.emit(recipe);
-  }
+
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,39 @@
+import { EventEmitter, Injectable, Output } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  private recipes : Recipe[] = [  //so that we can't directly access this array from outside
+    new Recipe(
+      'Simple Fried Chicken Breast Cutlets',
+      'A super-tasty fried chicken - just awesome!',
+      'https://www.thespruceeats.com/thmb/vXAnDRMRE4hhpENdSN6eyTsBeeg=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/simple-fried-chicken-breast-cutlets-3056859-Hero-5b8d4b3646e0fb0025e8f8ec.jpg',
+      [
+        new Ingredient('Meat',1),
+        new Ingredient('French Fries',20)
+      ]),
+    new Recipe(
+      'Pasta Al Pomodoro',
+      'Pasta with red saus - just awesome!',
+      'https://assets.epicurious.com/photos/56043a3e7c9cb37c518d5bde/9:4/w_1178,h_523,c_limit/365186_hires.jpg',
+      [
+        new Ingredient('Buns',2),
+        new Ingredient('Meat',1)
+      ])
+  ]
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
+  constructor(private slService:ShoppingListService) { }
+
+  getRecipes(){  //to access the array from outside
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients:Ingredient[]){
+    this.slService.addIngredients(ingredients);
+  }
+}
