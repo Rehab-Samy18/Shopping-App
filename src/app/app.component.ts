c5a17178804cfcae66c154de89b9fd71f47ad20f
rehab.samy <rehab.samy@xlab-group.com>
2022-09-12 09:17:25 +0200

    Adding Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-app';
-  loadedFeature = 'recipe'
-  onNavigate(feature:string){
-    this.loadedFeature = feature
-  }
 }
